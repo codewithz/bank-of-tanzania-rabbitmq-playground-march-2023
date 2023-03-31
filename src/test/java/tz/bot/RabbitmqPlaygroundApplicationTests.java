673ce408c3584f6a98017e0743b21eba9727cb45
Zartab M Nakhwa <zartab@codewithz.com>
2023-03-31 16:28:52 +0530

    Initial commit

---
+++ src/test/java/tz/bot/RabbitmqPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package tz.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
